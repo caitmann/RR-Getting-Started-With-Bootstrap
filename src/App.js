fbba9db93c730fdd0811330f13a3187a1defa3f3
caitmann <caitlynleemann@gmail.com>
2024-04-04 12:48:04 -0400

    initial commit, completed assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import './App.css';
-
-
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
 
 function App() {
   return (
     <div className="App">
+      <Nav activeKey="/home" fill variant="tabs">
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/about">About Us</Nav.Link>
+        </Nav.Item>
+      </Nav>
       <h1>Bootstrap Website</h1>
-     
+      <Button variant="info">I'm a button</Button>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Form Dropdown with Favorite Movies</Form.Label>
+          <Form.Control as="select">
+            <option>Pick a movie</option>
+            <option>Harry Potter</option>
+            <option>Star Wars</option>
+            <option>Avengers</option>
+            <option>Chronicles of Narnia</option>
+            <option>Matrix</option>
+          </Form.Control>
+        </Form.Group>
+        <Button variant="primary" type="submit">Submit</Button>
+      </Form>
     </div>
   );
 }
